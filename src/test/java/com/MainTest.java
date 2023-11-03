79ba46c6d556841cecbf7cebd7f1b5858821b3f9
JustQJ <37905360+JustQJ@users.noreply.github.com>
2023-11-03 14:50:05 +0800

    test3

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -10,15 +10,15 @@ class MainTest {
 	void test() {
 		Main main = new Main();
 		int result = main.add(1, 1);
-		int rr = main.add(3,-1);
+		
 		assertEquals(2, result);
 	}
 	@Test
 	void test1() {
 		Main main = new Main();
 		// int result = main.add(1, 1);
-		int rr = main.add(3,-1);
-		assertEquals(2, rr);
+		int rr = main.add(3,4);
+		assertEquals(7, rr);
 	}
 
 }
