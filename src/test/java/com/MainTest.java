6efd6ccf779dd6a8fbdba5e7de93f85f15dfc613
JustQJ <37905360+JustQJ@users.noreply.github.com>
2023-11-03 14:46:13 +0800

    ffff

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -10,7 +10,7 @@ class MainTest {
 	void test() {
 		Main main = new Main();
 		int result = main.add(1, 1);
-		int result = main.add(3,4);
+		int result = main.add(3,-1);
 		assertEquals(2, result);
 	}
 
